a677d218db17092e91fcb6c238a10ad09206108f
JeongJunSeo99 <wnstj0810@naver.com>
2024-09-06 17:23:17 +0900

    feat: 회원 권한 추가

---
+++ src/main/java/com/swcamp9th/springsecuritypratice/member/command/domain/aggregate/entity/RoleMember.java
@@ -0,0 +1,30 @@
+package com.swcamp9th.springsecuritypratice.member.command.domain.aggregate.entity;
+
+import com.swcamp9th.springsecuritypratice.member.command.domain.aggregate.RoleMemberPk;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tbl_member_role")
+@IdClass(RoleMemberPk.class)
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RoleMember {
+
+    @Id
+    @Column(name = "member_id")
+    private int memberId;
+
+    @Id
+    @Column(name = "role_id")
+    private int roleId;
+}
