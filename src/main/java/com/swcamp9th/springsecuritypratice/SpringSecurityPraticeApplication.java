db2fb12bd354be6ed2ed21654086257fcdcca5ed
JeongJunSeo99 <wnstj0810@naver.com>
2024-09-06 15:19:13 +0900

    chore: build.gradle에 의존성 추가

---
+++ src/main/java/com/swcamp9th/springsecuritypratice/SpringSecurityPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.swcamp9th.springsecuritypratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPraticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPraticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swcamp9th/springsecuritypratice/SpringSecurityPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcamp9th.springsecuritypratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPraticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
